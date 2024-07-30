97f0d823fb4004e8e0e63e5a30a8df7a3607a365
asius20 <123886881+asius20@users.noreply.github.com>
2024-07-30 20:23:56 +0100

    Salam Alaykum working

--- app/src/main/java/com/example/littlelemon2/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon2/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.littlelemon2.ui.theme.LittleLemon2Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,14 +16,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LittleLemon2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            mainPanel("\n \nSalam alaykum world!")
         }
     }
 }
@@ -35,6 +26,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+    )
+}
+
+@Composable
+fun mainPanel(name: String){
+    Text(text = name,
+        fontSize = 32.sp ,
     )
 }
 
