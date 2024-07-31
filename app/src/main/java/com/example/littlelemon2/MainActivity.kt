3ddf49aaffe02ed5c40dad8dcf29530f9fcace56
asius20 <123886881+asius20@users.noreply.github.com>
2024-07-31 03:21:06 +0100

    resolving the problem of string Resource

--- app/src/main/java/com/example/littlelemon2/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon2/MainActivity.kt
@@ -7,16 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.littlelemon2.ui.theme.LittleLemon2Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            mainPanel("\n \nSalam alaykum world!")
+            RestaurantName("Hello! \n"+stringResource(R.string.title))
         }
     }
 }
@@ -29,17 +31,26 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Composable
-fun mainPanel(name: String){
-    Text(text = name,
-        fontSize = 32.sp ,
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LittleLemon2Theme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun RestaurantName(name: String){
+    Text(text = name,
+        fontSize = 28.sp ,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RestaurantNamePreview() {
+    val title = stringResource(R.string.title)
+    LittleLemon2Theme {
+        RestaurantName(title)
     }
 }
